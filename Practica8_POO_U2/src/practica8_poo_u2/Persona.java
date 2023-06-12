874d31bb336c5b3a106ec9508c800533da888cf9
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-06-12 08:59:30 -0500

    Add files via upload

---
+++ Practica8_POO_U2/src/practica8_poo_u2/Persona.java
@@ -0,0 +1,16 @@
+
+package practica8_poo_u2;
+
+public class Persona {
+    
+    String nombre;
+    //Creacion de un constructor
+    public Persona (String nombre){
+        this.nombre=nombre;
+    }
+
+    public void setNombre(String nom) {
+        nombre = nom;
+    }
+    
+}
