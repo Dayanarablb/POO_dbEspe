7147f88b6861ff69721856cc8c40f8ed445b4d06
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-06-30 11:38:47 -0500

    Add files via upload

---
+++ POO_U2_P11_SOBREESCRITURA/src/poo_u2_p11_sobreescritura/Persona.java
@@ -0,0 +1,14 @@
+
+package poo_u2_p11_sobreescritura;
+
+import animal.Animal;
+
+public class Persona extends Animal {//Me va a salir error entonces tengo que importar la clase Animal que esta en el paquete animal 
+  @Override
+    //Metodo
+    public void Comer(){
+        System.out.println( " SOY UN HUMANO MI IQ eS 150 ");//IQ Coeficiente intelectual
+        
+        
+    }
+}
