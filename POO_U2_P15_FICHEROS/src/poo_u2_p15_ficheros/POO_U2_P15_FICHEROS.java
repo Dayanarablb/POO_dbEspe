e5bab6a004ac5d889146fb0c9aca5c4b45e2bc0b
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-07-07 10:04:30 -0500

    Add files via upload

---
+++ POO_U2_P15_FICHEROS/src/poo_u2_p15_ficheros/POO_U2_P15_FICHEROS.java
@@ -0,0 +1,23 @@
+
+package poo_u2_p15_ficheros;
+
+import java.io.*;
+import static poo_u2_p15_ficheros.Archivo.crearArchivo;
+
+public class POO_U2_P15_FICHEROS {
+
+    
+    public static void main(String[] args) {
+        
+        crearArchivo("archivo\\miarchivo.txt"); //Alt 92 para poner estas barritas
+        
+       
+        
+        
+        
+    
+        
+
+    }
+    
+}
