8004cf7272452a1b91b59b70c512371dea3d4890
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-07-07 22:02:49 -0500

    Operadores

---
+++ POO_U1_P3_OPERADORES/src/operadores_poo/OPERADORES_POO.java
@@ -0,0 +1,35 @@
+package operadores_poo;
+
+
+public class OPERADORES_POO {
+
+    
+    public static void main(String[] args) {
+        //declaramos las variables y las inicializamos
+        int a=20;
+        int b=15;
+        int c=16;
+        int d=740;
+        
+        //Operaciones
+       //operacion suma 
+       int suma=a+b+c+d;
+       //Operacion resta
+       int resta=d-c;
+       //operacion multiplicacion
+       int multiplicacion=d*a;
+       //operacion division 
+       float division =d/b;
+       //Resultados 
+       //Imprime el resultado de la suma
+       System.out.println("La suma es: " +suma);
+       //IMPRIME EL RESULTADO DE LA RESTA
+       System.out.println("La resta es:" +resta);
+       //IMPRIMER EL RESULTADO DE LA MULTIPLICACION
+       System.out.println( "La multiplicacion es: " + multiplicacion);
+       //IMPRIME EL RESULTADO DE LA DIVISION 
+       System.out.println("La division es: " + division);
+       
+    }
+    
+}
