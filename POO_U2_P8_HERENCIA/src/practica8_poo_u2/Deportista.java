8c342bbee437b65380a17d95e15967b3e5e37cdb
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-07-07 22:13:10 -0500

    Herencia

---
+++ POO_U2_P8_HERENCIA/src/practica8_poo_u2/Deportista.java
@@ -0,0 +1,15 @@
+
+package practica8_poo_u2;
+
+public class Deportista extends Persona{
+    String Deporte;
+ //Creamos constructor   
+ public Deportista(String nom){
+     super(nom);
+    
+ }
+ public void cambiarNombre(String nom){
+     
+ }
+    
+}
---
+++ POO_U2_P8_HERENCIA/src/practica8_poo_u2/Practica8_POO_U2.java
@@ -0,0 +1,15 @@
+
+package practica8_poo_u2;
+
+
+public class Practica8_POO_U2 {
+
+    
+    public static void main(String[] args) {
+        Persona pr = new Persona();
+        Deportista dep = new Deportista();
+      
+        
+    }
+    
+}
