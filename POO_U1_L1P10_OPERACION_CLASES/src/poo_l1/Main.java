5d901e4617e347a897b575a35ed714780a84b6e9
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-07-07 21:59:08 -0500

    Practica U1

---
+++ POO_U1_L1P10_OPERACION_CLASES/src/poo_l1/Main.java
@@ -0,0 +1,15 @@
+
+package poo_l1;
+
+
+public class Main {
+    public static void main (String [] args){
+        Operacion ope = new Operacion();
+        ope.capturarNumeros();
+        ope.sumar();
+        ope.restar();
+        ope.division();
+        ope.multiplicacion();
+        ope.mostrarResultados();
+    }
+}
