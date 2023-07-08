713e56993abf1fb7e4fad92e20d7a8b6f86dcbd5
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-07-07 22:07:07 -0500

    Herencia

---
+++ POO_U2_HERENCIA/src/P1herencia/Motocicleta.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package P1herencia;
+
+/**
+ * @author ESPE
+ *
+ */
+public class Motocicleta extends Vehiculo{
+
+	boolean patada;//para que herede los atributos de la clase padre tenemos que poner despues del nombre m,otocicleta el comando extends y lllamammos a la clase vehiculo
+    //estoy diciendo que es una extencion de la clase vehiculo
+    @Override//comado que dice que estamos herendando estos metodos de la clase padre (Vehiculo) nos dice que los podemos utilizar 
+    public void encendido (){
+    encendido = true;
+    estado = "Motocicleta encendida";
+    }
+    //metodo apagado
+    public void apagado(){
+    encendido = false;
+    estado = "Motocicleta apagada";
+    }
+
+}
