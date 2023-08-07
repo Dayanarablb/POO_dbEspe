f66aaf2cc47f3fd83829844c261a65ba4c7e957a
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-08-07 00:18:26 -0500

    CRUD

---
+++ POO_U3_P1_CRUD_DAYANARA_BAUTISTA/src/control/Conexion.java
@@ -0,0 +1,44 @@
+package control;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+    private static final String driver="com.mysql.jdbc.Driver";
+    private static final String user="root";
+    private static final String pass="";
+    private static final String url="jdbc:mysql://localhost:3306/estudiantes";
+    private static final String urlp="jdbc:mysql://localhost:3306/profesores";
+    private static final String urlh="jdbc:mysql://localhost:3306/horarios";
+    
+    Connection conectar = null;
+    //Coneccion con la base de datos
+    public Connection conexion(){
+        
+        try{
+            Class.forName(driver);
+            conectar=(Connection) DriverManager.getConnection(url,user,pass);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"error de conexion "+e.getMessage());
+        }
+        return conectar;
+    }
+}
+
+
+
+/* Connection conectar = null;
+    //Coneccion con la base de datos
+    public Connection conexion(){
+        
+        try{
+            Class.forName(driver);
+            conectar=(Connection) DriverManager.getConnection(url,user,pass);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"error de conexion "+e.getMessage());
+        }
+        return conectar;
+    }
+}*/
