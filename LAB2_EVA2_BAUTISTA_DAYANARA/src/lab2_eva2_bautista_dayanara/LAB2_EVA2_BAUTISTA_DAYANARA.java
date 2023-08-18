2d8c43d571e9c0b431f1bb833a7cbde429e63c6d
Dayanarablb <132688654+Dayanarablb@users.noreply.github.com>
2023-08-18 11:02:49 -0500

    Add files via upload

---
+++ LAB2_EVA2_BAUTISTA_DAYANARA/src/lab2_eva2_bautista_dayanara/LAB2_EVA2_BAUTISTA_DAYANARA.java
@@ -0,0 +1,16 @@
+
+package lab2_eva2_bautista_dayanara;
+
+import Vista.Estudiantes;
+
+public class LAB2_EVA2_BAUTISTA_DAYANARA {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiantes registro=new Estudiantes();
+        registro.setVisible(true);
+        
+    }
+    
+}
